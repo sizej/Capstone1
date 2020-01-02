776c1c9207b071fcc6a33bb8d9ea369fbe08d9ce
John Sizemore <size@Johns-MacBook-Pro.local>
2020-01-02 13:53:36 -0700

    initial exploration

--- src/movies.py
+++ src/movies.py
@@ -4,9 +4,14 @@ import matplotlib.pyplot as plt
 
 
 
+movie_df = pd.read_csv()
 
-
-
+# import movies csv and filter down by following criteria:
+# 1. Has a USA gross income
+# 2. Has a budget
+# 3. Produced by a major or mid-major studio (this could also be
+#  done with budget minimum or gross income minimum [though this would
+#  require an inflation adjusted figure])
 
 if __name__ == '__main__':
     pass
